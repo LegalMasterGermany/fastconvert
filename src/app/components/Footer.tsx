c55ede7d4c44cac4ed67187e1e597df799aa93f8
pooruser123 <germanuser88@tutanota.com>
2024-05-12 01:28:49 +0200

    Footer Update

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className='border-t text-center py-4'>
+    <footer className='border-t text-center py-4 absolute bottom-0 w-full'>
       <p>&copy; {new Date().getFullYear()} Fastconvert</p>
     </footer>
   );
