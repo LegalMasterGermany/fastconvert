c94ce1054ecd4b677b2ea85b8ebcd68c4903611a
pooruser123 <germanuser88@tutanota.com>
2024-05-11 23:30:12 +0200

    navbar

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -41,7 +41,7 @@ export default function NavBar() {
         </NavbarItem>
         <NavbarItem>
           <Link color="foreground" href="#">
-            About Us
+            API
           </Link>
         </NavbarItem>
       </NavbarContent>
